e74dcc3f7b9e67cf708ee8f6ac4e0dd7bf8d1afa
IbrahimDayax <ibthedev@gmail.com>
2024-03-19 15:32:20 +0800

    can successfully create new disk file and  boot into already existing disk file. However can only open files that were created and stored in memory. No persistance.

--- LibFS.c
+++ LibFS.c
@@ -13,11 +13,11 @@
 #define NO_OF_FILEDESCRIPTORS 256   // Define number of file descriptors
 
 // Function prototypes
-int read_block(int block, char* buf);
-int block_write(int block, char *buf, int size_to_wrt, int start_pos_to_wrt);
 int FS_Sync();
 void disk_Set();
 int FS_Boot(char *path);
+int find_file_in_directory(char *filename);
+void add_file_to_directory(char *filename, int inode_num);
 
 // Data structures
 struct inode {
@@ -46,54 +46,66 @@ struct super_block {
 };
 
 // Global variables
-struct inode inode_arr[NO_OF_INODES];          // Array of inodes
-struct DirectoryEntry dir_struct_entry[NO_OF_INODES];   // Array of directory entries
-struct super_block sup_block;                   // Super block structure
-char disk_name[100];                            // Disk image file name
-char filename[16];                              // File name
-int directory_map[NO_OF_INODES];                // Array to store directory entries (filename -> inode number)
-int free_Db_list[DISK_BLOCKS];                   // Array to store free data blocks
-int free_Inode_list[NO_OF_INODES];              // Array to store free inodes
-int fileDescriptor_map[NO_OF_FILEDESCRIPTORS][2]; // Array to store file descriptors (fd -> [Inode number, file pointer])
-int osErrno;
+int free_Inode_list_count = NO_OF_INODES;       // Count of free inodes
+int free_Db_list_count = DISK_BLOCKS;           // Count of free data blocks
+struct inode inode_arr[NO_OF_INODES] = {0};     // Array of inodes
+struct DirectoryEntry dir_struct_entry[NO_OF_INODES] = {0};   // Array of directory entries
+struct super_block sup_block = {0};                   // Super block structure
+char disk_name[100] = "";                            // Disk image file name
+int directory_map[NO_OF_INODES] = {0};                // Array to store directory entries (filename -> inode number)
+int free_Db_list[DISK_BLOCKS] = {0};                   // Array to store free data blocks
+int free_Inode_list[NO_OF_INODES] = {0};              // Array to store free inodes
+int fileDescriptor_map[NO_OF_FILEDESCRIPTORS][2] = {{0}}; // Array to store file descriptors (fd -> [Inode number, file pointer])
+int free_FD_list[NO_OF_FILEDESCRIPTORS] = {0};        // Array to store free file descriptors
+int osErrno = 0;
+int openfile_count = 0;                          // Count of open files
 
 // Function to synchronize file system
 int FS_Sync() {
     printf("FS_Sync\n");
 
     FILE *fp = fopen(disk_name, "rb+");    // Open disk image file for reading and writing in binary mode
+    if (fp == NULL) {
+        perror("Error opening disk image file");
+        exit(EXIT_FAILURE);
+    }
 
     // Retrieve super block from virtual disk and store into global super_block structure
-    char sup_block_buff[sizeof(sup_block)];
-    memset(sup_block_buff, 0, sizeof(sup_block));
+    char sup_block_buff[sizeof(sup_block)] = {0};
     fread(sup_block_buff, 1, sizeof(sup_block), fp);
     memcpy(&sup_block, sup_block_buff, sizeof(sup_block));
 
     // Retrieve directory structure block from virtual disk and store into global dir_entry structure
     fseek(fp, (sup_block.firstDbOfDir) * BLOCK_SIZE, SEEK_SET);
-    char dir_buff[sizeof(dir_struct_entry)];
-    memset(dir_buff, 0, sizeof(dir_struct_entry));
+    char dir_buff[sizeof(dir_struct_entry)] = {0};
     fread(dir_buff, 1, sizeof(dir_struct_entry), fp);
     memcpy(dir_struct_entry, dir_buff, sizeof(dir_struct_entry));
 
     // Retrieve inode block from virtual disk and store into global inode structure
     fseek(fp, (sup_block.startingInodesDb) * BLOCK_SIZE, SEEK_SET);
-    char inode_buff[sizeof(inode_arr)];
-    memset(inode_buff, 0, sizeof(inode_arr));
+    char inode_buff[sizeof(inode_arr)] = {0};
     fread(inode_buff, 1, sizeof(inode_arr), fp);
     memcpy(inode_arr, inode_buff, sizeof(inode_arr));
 
     // Storing all filenames into array
-    for (int i = NO_OF_INODES - 1; i >= 0; --i)
-        if (sup_block.inode_Bitmap[i] == 1)
-            directory_map[dir_struct_entry[i].inode_num] = i;
-        else
-            free_Inode_list[i] = i;
+    for (int i = NO_OF_INODES - 1; i >= 0; --i) {
+        if (i >= 0 && i < NO_OF_INODES) {
+            if (sup_block.inode_Bitmap[i] == 1) {
+                directory_map[dir_struct_entry[i].inode_num] = i;
+            } else {
+                free_Inode_list[i] = i;
+            }
+        }
+    }
 
     // Populate free_Db_list
-    for (int i = DISK_BLOCKS - 1; i >= sup_block.starting_DB_Index; --i)
-        if (sup_block.datablock_Bitmap[i] == 0)
-            free_Db_list[i] = i;
+    for (int i = DISK_BLOCKS - 1; i >= sup_block.starting_DB_Index; --i) {
+        if (i >= sup_block.starting_DB_Index && i < DISK_BLOCKS) {
+            if (sup_block.datablock_Bitmap[i] == 0) {
+                free_Db_list[i] = i;
+            }
+        }
+    }
 
     printf("Disk is mounted now\n");
 
@@ -102,29 +114,36 @@ int FS_Sync() {
     return 1;
 }
 
-// Function to initialize the disk
 void disk_Set() {
-    char buffer[BLOCK_SIZE];    // Buffer for writing to disk
+    FILE *fp;
+    char buffer[BLOCK_SIZE];
+    struct super_block sup_block = {0}; // Initialize to all zeros
 
-    FILE *fp = fopen(disk_name, "wb"); // Open disk image file for writing in binary mode
+    // Open disk image file for writing in binary mode
+    fp = fopen(disk_name, "wb");
+    if (fp == NULL) {
+        perror("Error opening disk image file");
+        exit(EXIT_FAILURE);
+    }
 
-    memset(buffer, 0, BLOCK_SIZE);  // Initialize buffer to 0
+    // Initialize buffer to 0
+    memset(buffer, 0, BLOCK_SIZE);
 
     // Write 0 to all disk blocks
-    for (int i = 0; i < DISK_BLOCKS; ++i)
-        fwrite(buffer, 1, BLOCK_SIZE, fp);   // Write buffer to disk
-
-    // Initialize super block
-    memset(&sup_block, 0, sizeof(struct super_block));
+    for (int i = 0; i < DISK_BLOCKS; ++i) {
+        fwrite(buffer, 1, BLOCK_SIZE, fp);
+    }
 
     // Set bitmap for metadata blocks and data blocks
-    for (int i = 0; i < sup_block.starting_DB_Index; ++i)
-        sup_block.datablock_Bitmap[i] = 1;
-    for (int i = sup_block.starting_DB_Index; i < DISK_BLOCKS; ++i)
-        sup_block.datablock_Bitmap[i] = 0;
-    for (int i = 0; i < NO_OF_INODES; ++i)
-        sup_block.inode_Bitmap[i] = 0;
-
+    for (int i = 0; i < sup_block.starting_DB_Index; ++i) {
+        sup_block.datablock_Bitmap[i] = 1; // Metadata blocks
+    }
+    for (int i = sup_block.starting_DB_Index; i < DISK_BLOCKS; ++i) {
+        sup_block.datablock_Bitmap[i] = 0; // Data blocks
+    }
+    for (int i = 0; i < NO_OF_INODES; ++i) {
+        sup_block.inode_Bitmap[i] = 0; // Initialize inode bitmap
+    }
     // Initialize inode pointers
     for (int i = 0; i < NO_OF_INODES; ++i) {
         for (int j = 0; j < 30; j++) {
@@ -136,6 +155,10 @@ void disk_Set() {
     fseek(fp, 0, SEEK_SET);
     fwrite(&sup_block, 1, sizeof(struct super_block), fp);
 
+    // Store directory structure after super block into virtual disk
+    fseek(fp, (sup_block.firstDbOfDir) * BLOCK_SIZE, SEEK_SET);
+    fwrite(dir_struct_entry, 1, sizeof(dir_struct_entry), fp);
+
     // Store inode blocks after super block into virtual disk
     fseek(fp, (sup_block.startingInodesDb) * BLOCK_SIZE, SEEK_SET);
     fwrite(inode_arr, 1, sizeof(inode_arr), fp);
@@ -145,17 +168,16 @@ void disk_Set() {
     printf("Virtual Disk %s created successfully\n", disk_name);
 }
 
-// Function to boot the file system
 int FS_Boot(char *path) {
-    strcpy(disk_name, path);    // Copy disk image file path to global variable
-
+    strcpy(disk_name, path);
+    printf("disk name: %s\n", disk_name);
     printf("FS_Boot %s\n", disk_name);
 
-    // Check if disk image file already exists
+    // Check if the disk image file already exists
     if (access(disk_name, F_OK) != -1) {
         printf("The Disk already exists\n");
     } else {
-        // Initialize disk
+        // Disk image doesn't exist, initialize it
         if (Disk_Init() == -1) {
             printf("Disk_Init() failed\n");
             osErrno = E_GENERAL;
@@ -163,7 +185,13 @@ int FS_Boot(char *path) {
         }
 
         // Save disk image
-        Disk_Save(disk_name);
+        printf("Saving disk image as: %s\n", disk_name);
+        if (Disk_Save(disk_name) == -1) {
+            printf("Disk_Save() failed\n");
+            osErrno = E_GENERAL;
+            return -1;
+        }
+        printf("Disk image saved successfully\n");
 
         // Set up disk
         disk_Set();
@@ -172,14 +200,108 @@ int FS_Boot(char *path) {
     return 0;
 }
 
-int File_Create(char *file) {
+// Function to check if the file exists in the directory
+int find_file_in_directory(char *filename) {
+    for (int i = 0; i < NO_OF_INODES; i++) {
+        if (strcmp(dir_struct_entry[i].file_name, filename) == 0) {
+            return i; // Return inode number if file found
+        }
+    }
+    return -1; // Return -1 if file not found
+}
+
+// Function to add a file to the directory
+void add_file_to_directory(char *filename, int inode_num) {
+    for (int i = 0; i < NO_OF_INODES; i++) {
+        if (strlen(dir_struct_entry[i].file_name) == 0) { // Find empty slot in directory
+            strcpy(dir_struct_entry[i].file_name, filename);
+            dir_struct_entry[i].inode_num = inode_num;
+            return;
+        }
+    }
+}
+
+/*********************************************************************
+ File_Create function - it will create the blank file into disk      *
+**********************************************************************/
+// Function to create a file
+int File_Create(char *filename) {
     printf("FS_Create\n");
+
+    int nextin, nextdb;
+
+    // Check if the file already exists
+    if (find_file_in_directory(filename) != -1) {
+        printf("Error: File '%s' already exists.\n", filename);
+        return -1;
+    }
+
+    // Check if there are free inodes
+    if (free_Inode_list_count == 0) {
+        printf("Error: No more files can be created. Maximum number of files reached.\n");
+        return -1;
+    }
+
+    // Check if there are free data blocks
+    if (free_Db_list_count == 0) {
+        printf("Error: Memory is full.\n");
+        return -1;
+    }
+
+    // Get the next free inode and data block
+    nextin = free_Inode_list[--free_Inode_list_count];
+    nextdb = free_Db_list[--free_Db_list_count];
+
+    // Update inode properties
+    inode_arr[nextin].inode_num = nextin;
+    inode_arr[nextin].filesize = 0;
+    inode_arr[nextin].pointer[0] = nextdb;
+
+    // Update directory entry
+    add_file_to_directory(filename, nextin);
+
+    printf("File '%s' created successfully.\n", filename);
+
     return 0;
 }
 
-int File_Open(char *file) {
+/*********************************************************************
+ File_Open - it will open the file                                   *
+**********************************************************************/
+int File_Open(char *filename) {
     printf("FS_Open\n");
-    return 0;
+
+    int inode_no = find_file_in_directory(filename);
+
+    // Check if the file exists in the directory
+    if (inode_no == -1) {
+        printf("Error: File '%s' not found.\n", filename);
+        return -1;
+    }
+
+    // Check if there are available file descriptors
+    if (openfile_count == NO_OF_FILEDESCRIPTORS) {
+        printf("Error: File descriptors are not available.\n");
+        return -1;
+    }
+
+    // Get a free file descriptor
+    int fd;
+    for (fd = 0; fd < NO_OF_FILEDESCRIPTORS; fd++) {
+        if (free_FD_list[fd] == 0) {
+            free_FD_list[fd] = 1;
+            break;
+        }
+    }
+
+    // Update file descriptor map
+    fileDescriptor_map[fd][0] = inode_no;
+    fileDescriptor_map[fd][1] = 0;
+    openfile_count++;
+
+    printf("File '%s' opened successfully. File descriptor: %d\n", filename, fd);
+
+    return fd;
 }
 
 int File_Read(int fd, void *buffer, int size) {
--- main.c
+++ main.c
@@ -1,106 +1,112 @@
-#include <stdio.h>      // Include standard input-output library
-#include <stdlib.h>     // Include standard library
-#include <string.h>     // Include string manipulation functions
-#include "LibFS.h"      // Include the custom filesystem library
+#include <stdio.h>
 
-#define block_size 512  // Define a constant for block size
+#include "LibFS.h"
 
-// Function to print text centered in a specified width
-void print_centered(const char *text) {
-    int width = 60;                 // Set the width of the output (adjust as needed)
-    int len = strlen(text);         // Get the length of the input text
-    int padding = (width - len) / 2; // Calculate padding needed for centering
-    // Print the centered text with appropriate padding
-    printf("%*s%s%*s\n", padding, "", text, padding, "");
+#define block_size 512
+void 
+usage(char *prog)
+{
+    fprintf(stderr, "usage: %s <disk image file>\n", prog);
+    exit(1);
 }
 
-// Function to print usage message and exit
-void usage(char *prog) {
-    fprintf(stderr, "usage: %s <disk image file>\n", prog);  // Print usage message to stderr
-    exit(1);    // Exit the program with error status
-}
-
-// Main function
-int main(int argc, char *argv[]) {
-    if (argc != 2) {    // Check if correct number of arguments are provided
-        usage(argv[0]); // Call usage function if arguments are incorrect
+int
+main(int argc, char *argv[])
+{
+    if (argc != 2) {
+	usage(argv[0]);
     }
-    char *path = argv[1];  // Get the path from command line arguments
-    char *buffer;          // Declare buffer variable
-    int status, count, size, choice, fd, offset; // Declare variables for status, count, size, file descriptor, and offset
-    char filename[16];     // Declare array to store filename
-    FS_Boot(path);         // Initialize filesystem with specified path
-    FS_Sync();             // Synchronize filesystem
-    printf("Disk Initialized\n"); // Print message indicating disk initialization
-    while (1) {                     // Start infinite loop for user interaction
-        printf("*************************************************************\n");
-        print_centered("Please enter your choice");  // Print centered message for user prompt
-        print_centered("0. Exit");                  // Print centered menu option for exiting
-        print_centered("1. File Create\t2. File Open\t3. File Read\t4. File Write");  // Print centered menu options for file operations
-        print_centered("5. File Seek\t6. File Close\t7. File Unlink");                 // Print centered menu options for file operations
-        print_centered("8. Dir Create\t9. Dir Read\t10. Dir Unlink");                  // Print centered menu options for directory operations
-        printf("*************************************************************\n\n");
-        scanf("%d", &choice);   // Read user choice
+    char *path = argv[1];
+    char *buffer;
+    int status,count,size,choice,fd,offset;
+    char filename [16];
+    FS_Boot(path);
+    FS_Sync();
+    printf("Disk Initialized\n");
+    while(1)
+    {	
+		printf("*************************************************************\n");
+	    printf("Please enter your choice\n");
+	    printf("0.Exit\n");
+	    printf("1.File Create\t2.File Open\t3.File Read\t4.File Write\n");
+	    printf("5.File Seek\t6.File Close\t7.File Unlink\n");
+	    printf("8.Dir Create\t9.Dir Read\t10.Dir Unlink\n");
+	    printf("*************************************************************\n\n");
+	    scanf("%d",&choice);
+	    
+	    //Menu selection cases
+	    switch(choice)
+	    {
+		    case 0: //Exit the program
+		    	printf("closing the system\n");
+		    	//unmounting();
+			    exit(0);
 
-        // Menu selection cases
-        switch (choice) {
-            case 0: // Exit the program
-                printf("closing the system\n"); // Print message indicating program termination
-                exit(0);                        // Exit the program
-            case 1: // File Create
-                printf("Enter the file name to create\n"); // Prompt user for filename
-                scanf("%s", filename);                      // Read filename from user
-                File_Create(filename);                      // Call function to create file
-                break;
-            case 2: // File Open
-                printf("Enter the file name to open\n");   // Prompt user for filename
-                scanf("%s", filename);                      // Read filename from user
-                File_Open(filename);                      // Call function to open file
-                break;
-            case 3: // File Read
-                printf("Enter the file name to read\n");  // Prompt user for filename
-                scanf("%s", filename);                      // Read filename from user
-                File_Read(fd, buffer, size);                      // Call function to read file
-                break;
-            case 4: // File Write
-                printf("Enter the file name to write\n"); // Prompt user for filename
-                scanf("%s", filename);                      // Read filename from user
-                // store_file_into_Disk(filename);
-                // count=File_Write(filename, buffer,size);
-                break;
-            case 5: // File Seek
-                File_Seek(fd, offset);                     // Call function to seek in file
-                break;
-            case 6: // File Close
-                printf("Enter the file name to close\n"); // Prompt user for filename
-                scanf("%s", filename);                      // Read filename from user
-                // File_Close(filename);
-                break;
-            case 7: // File Unlink
-                printf("Enter the file name to unlink\n"); // Prompt user for filename
-                scanf("%s", filename);                      // Read filename from user
-                status = File_Unlink(filename);              // Call function to unlink file
-                break;
-            // Directory Operations
-            case 8: // Dir Create
-                printf("Enter the dir path to create\n"); // Prompt user for directory path
-                scanf("%s", path);                          // Read directory path from user
-                status = Dir_Create(path);                   // Call function to create directory
-                break;
-            case 9: // Dir Read
-                printf("Enter the dir path to read\n");  // Prompt user for directory path
-                scanf("%s", path);                          // Read directory path from user
-                count = Dir_Read(path, buffer, size);          // Call function to read directory
-                break;
-            case 10: // Dir Unlink
-                printf("Enter the dir path to unlink\n"); // Prompt user for directory path
-                scanf("%s", path);                          // Read directory path from user
-                status = Dir_Unlink(path);                   // Call function to unlink directory
-                break;
-            default: // Invalid choice
-                printf("Invalid Choice!!! Try Again\n"); // Print error message for invalid choice
-        }
+		    case 1: //File Create
+		    	printf("Enter the file name to create\n");
+		    	scanf("%s",filename);
+			    File_Create(filename);
+			    break;
+
+		    case 2: //File Open
+		    	printf("Enter the file name to open\n");
+		    	scanf("%s",filename);
+		    	File_Open(filename);
+		    	break;
+
+		    case 3: //File Read
+		    	printf("Enter the file name to read\n");
+		    	scanf("%s",filename);
+		    	File_Read(fd, buffer,size);
+		    	break;
+
+		    case 4: //File Write
+		    	printf("Enter the file name to write\n");
+		    	scanf("%s",filename);
+		    	//store_file_into_Disk(filename);
+
+		    	//count=File_Write(filename, buffer,size);
+		    	break;
+
+		    case 5: //File Seek
+		    	File_Seek(fd,offset);
+		    	break;
+
+		    case 6: //File Close
+		    	printf("Enter the file name to close\n");
+		    	scanf("%s",filename);
+		    	//File_Close(filename);
+		    	break;
+
+		    case 7: //File Unlink
+		    	printf("Enter the file name to unlink\n");
+		    	scanf("%s",filename);
+		    	status=File_Unlink(filename);
+		    	break;
+
+		    //Directory Operations
+		    case 8: //Dir Create
+		    	printf("Enter the dir path to create\n");
+		    	scanf("%s",path);
+		    	status=Dir_Create(path);
+		    	break;
+
+		    case 9: //Dir Read
+		    	printf("Enter the dir path to read\n");
+		    	scanf("%s",path);
+		    	count=Dir_Read(path,buffer,size);
+		    	break;
+
+		    case 10: //Dir Unlink
+		    	printf("Enter the dir path to unlink\n");
+		    	scanf("%s",path);
+		    	status = Dir_Unlink(path);
+		    	break;
+
+		    default:
+		    	printf("Invalid Choice!!! Try Again\n");
+	    }
     }
 
-    return 0; // Return 0 to indicate successful execution
+    return 0;
 }
